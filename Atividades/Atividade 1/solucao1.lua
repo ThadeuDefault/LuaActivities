c985534a409b45664942d71ef4affdf1f2e06ed3
Thadeu Defaulti <thadeudefault@gmail.com>
2019-04-16 20:51:48 -0300

    Atualização de pacotes

---
+++ Atividades/Atividade 1/solucao1.lua
@@ -0,0 +1,15 @@
+n1, n2, n3 = io.read('*n','*n','*n')
+
+maior = 0
+
+if n1 > maior then
+			
+end
+
+if n2 > maior then
+
+end
+
+if n3 > maior then
+
+end
---
+++ Atividades/Atividade 1/solucao2.lua
@@ -0,0 +1,16 @@
+
+ N1, N2, N3 = io.read('n*', '*n', '*n')
+
+ if N1 > N2 then
+    if N1 > N3 then
+    
+    else
+    end
+ else
+  if N2 > N3 then
+	MaiorPar = N2%2 == 0
+  else 
+     MaiorPar = N3%2 == 0
+  end
+end
+
