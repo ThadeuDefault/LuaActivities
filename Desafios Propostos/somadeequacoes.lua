1379eea0871dd375ea0728018cc9e17ecd4c95c6
Thadeu Defaulti <thadeudefault@gmail.com>
2019-05-06 15:47:12 -0300

    Add files via upload

---
+++ Desafios Propostos/somadeequacoes.lua
@@ -0,0 +1,17 @@
+-- Fazer uma função que resolva o seguinte problema (a^2) + (3*b)
+
+-- Faz entrada dos números 'a' e 'b'
+
+a, b = io.read('*n','*n')
+
+-- Faz a função que resolve o problema (a^2) + (3*b), e printa a resposta:
+
+ function aeb(a, b)
+	print((a^2) + (3*b))
+end
+
+-- E por último, chamar a função:
+
+aeb(a,b)
+
+-- Prontinho!
